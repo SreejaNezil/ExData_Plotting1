0b1a59f02838059c5a5e03244e8b15ea34fb3f8e
Sreeja Nezil <sreeja.nezil@gmail.com>
2015-04-10 15:51:44 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,7 @@
+# Read only the required data from the file
+# First create a handle to the file and this can be used with sqldf-
+# - in order to select only the required data
+
 mydata <- "household_power_consumption.txt"
 myfile <- file(mydata)
 library(sqldf)
@@ -6,13 +10,18 @@ mydf <- sqldf("select * from myfile where Date in ('1/2/2007','2/2/2007')",
               file.format=list(header=TRUE, sep = ";"))
 close(myfile)
 
+# Create a new column in the data frame that combines both Date and Time 
+# Then covert the new column to Date format using strptime
 mydf$DateTime <- paste(mydf$Date,mydf$Time)
 mydf$DateTime <- strptime(mydf$DateTime,"%d/%m/%Y %H:%M:%S")
 
+#Create the file for plotting and set the size
 png(filename = "plot1.png", width = 480, height = 480, units = "px", 
     pointsize = 12,bg="white")
 
+# Plot the graph as required
 hist(mydf$Global_active_power, col="red",xlab = "Global Active Power (kilowatts)",
      main = "Global Active Power")
 
+# Close the device/ file
 dev.off()
